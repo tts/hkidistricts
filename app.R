97f2d81abdc55f6cce46df69f1829f8901bc058d
Tuija <tuija.sonkkila@aalto.fi>
2022-09-22 21:31:40 +0300

    Attributions, reduced help text

--- app.R
+++ app.R
@@ -5,15 +5,15 @@ library(sf)
 
 source("polygonangle.R")
 streets <- readRDS("streets.RDS")
-areas <- as.vector(sort(unique(streets$kaupunginosa)))
+areas <- as.vector(sort(c(unique(streets$kaupunginosa))))
 
-themes <- list("Dark" = theme_dark(),
-               "Light" = theme_light())
+themes <- list("Light" = theme_light(),
+               "Dark" = theme_dark())
 
 ui <- fluidPage(
   
   tags$h2(
-    HTML("Streets of Helsinki by district as minimum bounding boxes with the angle")
+    HTML("Streets of Helsinki as minimum bounding boxes with angle")
   ),
   
   sidebarPanel(
@@ -24,25 +24,16 @@ ui <- fluidPage(
                    multiple = TRUE,
                    options = list(
                      maxItems = 2,
-                     placeholder = 'Pick district(s)',
+                     placeholder = 'Pick 1-2 districts',
                      onInitialize = I('function() { this.setValue(""); }')
                    )),
-    selectizeInput(inputId = "theme",
-                   label = "Style",
-                   choices = names(themes),
-                   options = list(
-                     placeholder = 'and style',
-                     onInitialize = I('function() { this.setValue(""); }')
-                   )),
+    selectInput(inputId = "theme",
+                label = "Style",
+                choices = names(themes),
+                selected = "Light"),
     
     HTML("<p></p>
           <span style='color:black;font-size:12px'
-          <p>
-            Select 1-2 districts, and the plot style.
-          </p>
-          <p></p>
-          <p>
-          </p>
           <p><a href='https://github.com/tts/hkidistricts'>R code</a> by <a href='https://twitter.com/ttso'>@ttso</a>.</p>
           <p></p>
           <p>Data: <a href='https://hri.fi/data/en_GB/dataset/helsingin-kaupungin-yleisten-alueiden-rekisteri'>Register of public areas in the City of Helsinki</a>.</p>
@@ -52,16 +43,14 @@ ui <- fluidPage(
   mainPanel(
     tabsetPanel(
       tabPanel("Plot", 
-               plotOutput("plot", height = 400, width = "100%")),
-              # downloadButton('ExportPlot', 'Export as png')),
+               plotOutput("plot", width = "100%")),
       tabPanel("Map", 
-               leafletOutput("map", height = 400, width = "100%"))
+               leafletOutput("map", width = "100%"))
     ),
     width = 9
   ))
 
   
-
 server <- function(input, output, session) {
   
   plot_theme <- reactive({themes[[input$theme]]})
@@ -75,7 +64,7 @@ server <- function(input, output, session) {
   
   output$map <- renderLeaflet({
     leaflet(area_chosen()) %>% 
-      addTiles() %>% 
+      addTiles(attribution = "OpenStreetMap | Register of public areas in the City of Helsinki") %>% 
       addPolygons(weight = 1, 
                   color = "black")
   }) 
@@ -87,14 +76,16 @@ server <- function(input, output, session) {
       ggplot() +
       geom_sf(alpha = .8) +
       ggtitle(paste(input$area, collapse = " | ")) +
+      labs(caption = "Source: hri.fi Register of public areas in the City of Helsinki | @ttso ") +
       plot_theme() +
       theme(panel.grid.major = element_blank(),
             panel.grid.minor = element_blank(),
             axis.line = element_blank(),
             axis.ticks = element_blank(),
             axis.text = element_blank(),
-            plot.title = element_text(size = 20)) 
-  })
+            plot.title = element_text(size = 20),
+            plot.caption = element_text(margin = margin(20, 0, 0, 0)))
+    })
   
 }
 
