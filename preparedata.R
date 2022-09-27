118265aa110a5228e1cf8890a7c9f6b6794c80f2
Tuija <tuija.sonkkila@aalto.fi>
2022-09-27 16:16:07 +0300

    Importing plot of the whole city

--- preparedata.R
+++ preparedata.R
@@ -15,3 +15,33 @@ streets <- streets %>%
   st_transform(4326)
 
 saveRDS(streets, "streets.RDS")
+
+source("polygonangle.R")
+
+# Geographic orientation of all streets
+
+streets_geo <- streets %>% 
+  st_geometry() %>% 
+  st_as_sf() 
+
+allstreets <- pmap_dfr(streets_geo, min_box_sf) 
+
+allstreets$range <- cut(allstreets$angle, breaks=seq(0,180,30))
+
+range_count <- data.frame(allstreets$range) %>%
+  rename(range = allstreets.range) %>%
+  dplyr::count(., range)
+
+allstreets_range <- left_join(allstreets, range_count) %>% 
+  rename(Range = range)
+
+hki <- ggplot(allstreets_range, aes(x = angle, fill = Range)) + 
+  geom_histogram(breaks = seq(0, 180, 30), colour = "grey") + 
+  coord_polar(start = 0) + 
+  theme_minimal() + 
+  scale_fill_brewer() + 
+  ylab("Count") + 
+  scale_x_continuous("", limits = c(0, 180), breaks = seq(0, 180, 30), labels = seq(0, 180, 30))
+
+saveRDS(hki, "hki.RDS")
+
